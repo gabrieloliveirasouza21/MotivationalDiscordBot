2d342382ca6301df7e746a36d02ed2a52e3b13cd
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-09-06 23:01:08 -0300

    Frase_Motivacional#1 adicionada

--- TomMorelloCommands/TomMorello.cs
+++ TomMorelloCommands/TomMorello.cs
@@ -11,16 +11,16 @@ namespace MotivationalDiscordBot.TomMorelloCommands {
     internal class TomMorello:ApplicationCommandModule{
 
 
-        [SlashCommand("Teste", "mano so escreve ai na moral")]
+        [SlashCommand("sonhos", "essa é de pular de alegria")]
         public async Task Teste1(InteractionContext ctx) {
 
             await ctx.CreateResponseAsync(InteractionResponseType
                 .ChannelMessageWithSource,
                 new DiscordInteractionResponseBuilder()
-                .WithContent("Rodando esse krl..."));
+                .WithContent("Se liga na mensagem pae"));
 
             var embedMessage = new DiscordEmbedBuilder() {
-                Title = "Oloko bixo funcionou, agora vamos para os reclames do plim plim"
+                Title = "Não desista dos seus sonhos, deixe que eles desistam de você"
             };
 
             await ctx.Channel.SendMessageAsync(embed : embedMessage);
